91b637f4cbe90389815fd20b46726fadab3e0229
SG0307799 <kartik.madaan@sabre.com>
2020-05-04 18:36:44 +0530

    Updates

---
+++ precache-manifest.9bbf2016131cb7b5d47af4308c046813.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050799610ac80b7a87400a20005ad9bc",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "bae217c79307d5a03d6e",
+    "url": "/weather-react/static/css/main.ddd18547.chunk.css"
+  },
+  {
+    "revision": "b1aa83b68d302234402b",
+    "url": "/weather-react/static/js/2.f148cd4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.f148cd4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae217c79307d5a03d6e",
+    "url": "/weather-react/static/js/main.1d552fb8.chunk.js"
+  },
+  {
+    "revision": "4f7acb5d8418cb8d47f1",
+    "url": "/weather-react/static/js/runtime-main.5c5a32ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "913ffbc7477bd56f5dccfe54385ccf62",
+    "url": "/weather-react/static/media/sunny-day.913ffbc7.jpeg"
+  },
+  {
+    "revision": "b2eed64e7cd339d6e35d73454c34c5d7",
+    "url": "/weather-react/static/media/winter.b2eed64e.jpeg"
+  }
+]);
